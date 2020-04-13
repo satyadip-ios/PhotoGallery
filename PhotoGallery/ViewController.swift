7d81ee2cdb4767f31abb568a93c4f9b448d615bb
Satyadip Singha <satyads1@in.ibm.com>
2020-04-13 19:21:10 +0530

    Initial Commit

---
+++ PhotoGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoGallery
+//
+//  Created by Satyadip Singha on 13/04/2020.
+//  Copyright © 2020 Satyadip Singha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
