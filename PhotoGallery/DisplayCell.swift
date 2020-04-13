5d8049c64091cfa8599ea0090484881d6485dcb5
Satyadip Singha <satyads1@in.ibm.com>
2020-04-13 20:17:43 +0530

    UI design complete

---
+++ PhotoGallery/DisplayCell.swift
@@ -0,0 +1,24 @@
+//
+//  DisplayCell.swift
+//  PhotoGallery
+//
+//  Created by Satyadip Singha on 13/04/2020.
+//  Copyright © 2020 Satyadip Singha. All rights reserved.
+//
+
+import UIKit
+
+class DisplayCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PhotoGallery/DisplayItems.swift
@@ -0,0 +1,45 @@
+//
+//  DisplayItems.swift
+//  PhotoGallery
+//
+//  Created by Satyadip Singha on 13/04/2020.
+//  Copyright © 2020 Satyadip Singha. All rights reserved.
+//
+
+import UIKit
+
+class DisplayItems: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension DisplayItems: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as?  DisplayCell {
+            
+            //return cell
+        }
+        return UICollectionViewCell()
+    }
+}
